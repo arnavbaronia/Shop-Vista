788adc0021434517887eb92d8778559a3d9518f4
arnavbaronia <arnav.baronia@gmail.com>
2024-09-07 17:13:41 +0530

    db

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -3,12 +3,27 @@ import mongoose from "mongoose";
 export const connectDatabase = () => {
   let DB_URI = "";
 
-  if (process.env.NODE_ENV === "DEVELOPMENT") DB_URI = process.env.DB_LOCAL_URI;
-  if (process.env.NODE_ENV === "PRODUCTION") DB_URI = process.env.DB_URI;
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    DB_URI = process.env.DB_LOCAL_URI;
+  } else if (process.env.NODE_ENV === "PRODUCTION") {
+    DB_URI = process.env.DB_URI;
+  }
 
-  mongoose.connect(DB_URI).then((con) => {
-    console.log(
-      `MongoDB Database connected with HOST: ${con?.connection?.host}`
-    );
-  });
+  if (!DB_URI) {
+    console.error("Database URI not set. Please check your environment variables.");
+    process.exit(1); // Exit process with failure
+  }
+
+  mongoose
+    .connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((con) => {
+      console.log(`MongoDB Database connected with HOST: ${con.connection.host}`);
+    })
+    .catch((err) => {
+      console.error(`Error connecting to MongoDB: ${err.message}`);
+      process.exit(1); // Exit process with failure
+    });
 };
